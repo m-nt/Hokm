b812073b24296b13081aa188abc9baeeebd0aca7
mohamad nosratian <68176509+mohamadnosratian@users.noreply.github.com>
2021-08-07 21:18:59 +0430

    added basic io connection

---
+++ server.js
@@ -0,0 +1,21 @@
+const Express = require("express");
+const app = Express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("hello", (data) => {
+    console.log(data);
+    socket.emit("hi", { message: "hello back" });
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
